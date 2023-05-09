0249799d17f743a17c9610a06eb4f51767826b80
hoangan26022003 <132539604+hoangan26022003@users.noreply.github.com>
2023-05-10 00:49:40 +0700

    Add files via upload

---
+++ OP/b6_1/Main.java
@@ -0,0 +1,57 @@
+package OP.b6_1;
+
+public class Main {
+    public static void main(String[] args) {
+        Shape s1 = new Circle(5.5, "red", false);  // Upcast Circle to Shape
+        System.out.println(s1);                    // which version?
+        System.out.println(s1.getArea());          // which version?
+        System.out.println(s1.getPerimeter());     // which version?
+        System.out.println(s1.getColor());
+        System.out.println(s1.isFilled());
+        // System.out.println(s1.getRadius());  // Compilation error, Shape class does not have getRadius() method
+
+        Circle c1 = (Circle)s1;                   // Downcast back to Circle
+        System.out.println(c1);
+        System.out.println(c1.getArea());
+        System.out.println(c1.getPerimeter());
+        System.out.println(c1.getColor());
+        System.out.println(c1.isFilled());
+        System.out.println(c1.getRadius());
+
+        // Shape s2 = new Shape();  // Compilation error, Shape class is abstract and cannot be instantiated
+
+        Shape s3 = new Rectangle();   // Upcast
+        System.out.println(s3);
+        System.out.println(s3.getArea());
+        System.out.println(s3.getPerimeter());
+        System.out.println(s3.getColor());
+        // System.out.println(s3.getLength());  // Compilation error, Shape class does not have getLength() method
+
+        Rectangle r1 = (Rectangle)s3;   // downcast
+        System.out.println(r1);
+        System.out.println(r1.getArea());
+        System.out.println(r1.getColor());
+        System.out.println(r1.getLength());
+
+        Shape s4 = new Square(6.6);     // Upcast
+        System.out.println(s4);
+        System.out.println(s4.getArea());
+        System.out.println(s4.getColor());
+        // System.out.println(s4.getSide());  // Compilation error, Shape class does not have getSide() method
+
+        Rectangle r2 = (Rectangle)s4;  // Downcast to Rectangle, not Square
+        System.out.println(r2);
+        System.out.println(r2.getArea());
+        System.out.println(r2.getColor());
+        // System.out.println(r2.getSide());  // Compilation error, Rectangle class does not have getSide() method
+        System.out.println(r2.getLength());
+
+        Square sq1 = (Square)r2;  // Downcast Rectangle r2 to Square
+        System.out.println(sq1);
+        System.out.println(sq1.getArea());
+        System.out.println(sq1.getColor());
+        System.out.println(sq1.getSide());
+        System.out.println(sq1.getLength());  // This statement will output the same result as sq1.getSide() since Square is a subclass of Rectangle and inherits its getLength() method
+    }
+
+}
